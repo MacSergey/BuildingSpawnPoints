4c29b8aa5f1985510f5900a60d4b517efc2c41f5
MacSergey <macsergey@me.com>
2021-06-19 12:42:45 +0300

    Added release note

--- BuildingSpawnPoints/Properties/AssemblyInfo.cs
+++ BuildingSpawnPoints/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2fb06e50-a869-450b-be38-32529a8d960b")]
 
 #if BETA
-[assembly: AssemblyVersion("1.1.0.42")]
-[assembly: AssemblyFileVersion("1.1.0.32")]
+[assembly: AssemblyVersion("1.1.0.43")]
+[assembly: AssemblyFileVersion("1.1.0.33")]
 #else
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")] 
+[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyFileVersion("1.1")] 
 #endif
