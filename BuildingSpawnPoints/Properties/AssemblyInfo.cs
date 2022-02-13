f7787f9644f3c4418d659c4397b1cc068ee2c32b
MacSergey <macsergey@me.com>
2022-02-13 10:18:45 +0200

    Redesigned harmony is removed from allowed dependencies list

--- BuildingSpawnPoints/Properties/AssemblyInfo.cs
+++ BuildingSpawnPoints/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("2fb06e50-a869-450b-be38-32529a8d960b")]
 
 #if BETA
-[assembly: AssemblyVersion("1.2.0.70")]
-[assembly: AssemblyFileVersion("1.2.0.70")]
+[assembly: AssemblyVersion("1.2.0.71")]
+[assembly: AssemblyFileVersion("1.2.0.71")]
 #else
-[assembly: AssemblyVersion("1.2.3.1")]
-[assembly: AssemblyFileVersion("1.2.3.1")] 
+[assembly: AssemblyVersion("1.2.3.2")]
+[assembly: AssemblyFileVersion("1.2.3.2")] 
 #endif
